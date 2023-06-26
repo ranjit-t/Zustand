5cef5fa7d31f0357f4e03e4074cef0e29dc89198
Ranjit <amailtoranjith@gmail.com>
2023-06-26 10:34:10 +0200

    fetching data to set state

--- Zustand-Store-Basics/src/Zustand/store.jsx
+++ Zustand-Store-Basics/src/Zustand/store.jsx
@@ -4,7 +4,10 @@ export const useCountStore = create((set) => ({
   count: 0,
   name: "Ranjith",
   credits: "All Rights @ Ranjit Thota",
+  games: [],
+
   increaseCount: () => set((state) => ({ count: state.count + 1 })),
   decreaseCount: () => set((state) => ({ count: state.count - 1 })),
   resetCount: () => set({ count: 0 }),
+  gamesData: (data) => set((state) => (state.games = data)),
 }));
